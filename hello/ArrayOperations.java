fa7950dd72cb4932fed6598c53bc56441637fb1c
adriannabanana <afu4599@gmail.com>
2015-12-01 14:27:38 -0600

    ARRAY OPS

--- hello/ArrayOperations.java
+++ hello/ArrayOperations.java
@@ -23,4 +23,57 @@ public class ArrayOperations
         }
         values[0]=last;
     }
+    
+    public void evenToZero()
+    {
+        for (int i: values)
+        {
+            if (i%2 == 0)
+            {
+                values[i] = 0;
+            }
+        }
+    }
+    
+    public void largerNeighbor()
+    {
+        int[] original = values;
+        for (int i = 1; i < values.length-1; i++)
+        {
+            int neighbor1 = original[i-1];
+            int neighbor2 = original[i+1];
+            if (neighbor1>neighbor2)
+            {
+                values[i] = neighbor1;
+            }
+            else if (neighbor2>neighbor1)
+            {
+                values[i] = neighbor2;
+            }
+        }
+    }
+    
+    public void removeMiddle()
+    {
+
+        if (values.length%2==0)
+        {
+            int[] removedMiddle = new int[values.length-2];
+            int middle = values.length/2;
+            for (int i = 0; i < values.length; i++)
+            {
+                if ( i== middle || (i == (middle+1)))
+                {
+                }
+                else
+                {
+                    removedMiddle[i] = values[i];
+                }
+            }
+                    
+            
+             
+        }
+    }
+    
 }
