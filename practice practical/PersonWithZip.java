b59946963c145317e4af4b626eb6dc0503de3b10
adriannabanana <afu4599@gmail.com>
2015-12-17 15:09:06 -0600

    practical yee

---
+++ practice practical/PersonWithZip.java
@@ -0,0 +1,65 @@
+
+
+/**
+ * Write a description of class PersonWithZip here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PersonWithZip
+{
+    private String firstName;
+    private String lastName;
+    private int zipCode;
+
+    /**
+     * Default constructor for objects of class PersonWithZip
+     */
+    public PersonWithZip(String firstName, String lastName, int zipCode)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.zipCode = zipCode;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+
+    //returns the zipcode
+    public int getZip()
+    {
+        return this.zipCode;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public String toString()
+    {
+        // put your code here
+        return;
+
+    }
+
+}
